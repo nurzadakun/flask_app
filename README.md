a9d5080b2c83488709f5417099d44d72d657fd66
nurzadakun <nurzada670@gmail.com>
2023-03-15 15:41:40 +0600

    textcommit

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ git remote add origin https://github.com/nurzadakun/flask_app.git
 git push origin master
 git checkout -b alinabranch
 git branch
+
+
+set FLASK_APP=newproj
+set FLASK_ENV=development
+flask run
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+users = {'login':'password', 'nurzada':'parol'}
 
-@app.route('/')
-def hello():
-    return 'Hello,  World!'
+@app.route("/")
+def welcome():
+    return render_template("index.html", name="1")
 
-@app.route('/')
-def help():
-    return 'Hi Nurzada!'
+@app.route("/auth", methods=['GET','POST'])
+def auth():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        
+        if(users[login]==password):
+            return render_template("welcome.html", login=login)
+    return render_template("auth.html")
+
+
