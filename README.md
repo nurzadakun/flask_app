985114fd21867bcd337b53bf6272ad56bd145028
nurzadakun <nurzada670@gmail.com>
2023-03-16 20:46:39 +0600

    textcommit

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ git init
 git add .
 git commit -m  "textcommit"
 git remote add origin https://github.com/nurzadakun/flask_app.git
-git push origin master
+git push origin nurzada
 git checkout -b alinabranch
 git branch
 
@@ -11,3 +11,7 @@ git branch
 set FLASK_APP=main
 set FLASK_ENV=development
 flask run
+
+
+
+wxtcbdbyciknjgqt
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request
 import sqlite3
 import hashlib
+import random
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'nurzada670@gmail.com'
+app.config['MAIL_PASSWORD'] = 'wxtcbdbyciknjgqt'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
 
 @app.route("/")
 def welcome():
@@ -13,23 +24,25 @@ def welcome():
 @app.route("/regist", methods=['GET','POST'])
 def regist():
     if request.method == 'POST':
+        email = request.form['email']
         login = request.form['login']
         password = request.form['password']
-        email = request.form['email']
+
+        password = hash_password(password)
 
         connect = sqlite3.connect('users.db')
         cursor = connect.cursor()
 
         cursor.execute('''INSERT INTO users (login, email, password)
-        VALUES (?, ?, ?)
-        ''', [login, password, email])
+        VALUES ('%s', '%s', '%s')
+        '''%(login, email, password))
 
         connect.commit()
         connect.close()
     return render_template("regist.html")
 
 def hash_password(password):
-    password = hashlib.sha256(password)
+    password = hashlib.sha256(password.encode('utf-8')).hexdigest()
     return password
 
 
@@ -39,12 +52,15 @@ def auth():
         login = request.form['login']
         password = request.form['password']
 
+        password = hash_password(password)
+
         connect = sqlite3.connect('users.db')
         cursor = connect.cursor()
 
-        cursor.execute('SELECT * FROM USERS WHERE login=?', [login])
-        cursor.execute('SELECT * FROM USERS WHERE password=?', [password])
+        cursor.execute("SELECT * FROM USERS WHERE login='%s' AND password='%s'"%(login, password))
         user = cursor.fetchall()
+
+        print(user)
 
         if user:
             if(user[0][3]==password):
@@ -53,4 +69,20 @@ def auth():
         connect.commit()
         connect.close()
     return render_template("auth.html")
+
+def generate_random_number():
+    number = random.randint(10000, 99999)
+    return number
+
+
+@app.route("/forgot_password", methods=['GET','POST'])
+def forgot():
+    if request.method == 'POST':
+        email = request.form['email']
+
+        msg = Message('Восстановление пароля', sender = 'flask_first_app', recipients = [email])
+        msg.body = "Перейдите по ссылке чтобы восстановить пароль"
+        mail.send(msg)
+
+    return render_template("forgotpassword.html")
 
