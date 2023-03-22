9c537259c70fafa54067e83deff59fcac72b337b
nurzadakun <nurzada670@gmail.com>
2023-03-22 19:28:35 +0600

    textcommit

---
+++ mail_config.py
@@ -0,0 +1,15 @@
+from flask_mail import Mail
+from main import app
+
+
+def mail_config(app):
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USERNAME'] = 'nurzada670@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'wxtcbdbyciknjgqt'
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
+
+def mail_set(app):
+    mail = Mail(app)
+    return mail
